36c80551af8b32c5e8b9eadc66a32fda69969607
kaian0n <linsenmannalex@proton.me>
2026-01-26 13:53:55 -0700

    maybe

--- hosts/common/users/al.nix
+++ hosts/common/users/al.nix
@@ -1,26 +1,25 @@
-
 # /hosts/common/users/al.nix
 { config, pkgs, inputs, ... }:
 {
-   users.users.al = {
-      initialHashedPassword = "$y$j9T$hkbcNcSVU6meI8SVcz2/10$iElppu6DicE7qd3CKiVISN47Ep/IaJoeJL4pvMns9.3";
-      isNormalUser = true;
-      description = "al";
-      extraGroups = [
-         "wheel"
-         "networkmanager"
-         "libvirtd"
-         "flatpak"
-         "audio"
-         "video"
-         "plugdev"
-         "input"
-         "qemu-libvirtd"
-      ];
-      openssh.authorizedKeys.keys = [
-         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqyyU56DPcUBpsvOxSZmLSvfxj5ltrNNR0sFMeOe4Xl alex@alns"
-      ];
-   };
-
-   home-manager.users.al = import ../../../home/al/${config.networking.hostName}.nix;
+  users.users.al = {
+    initialHashedPassword = "$y$j9T$hkbcNcSVU6meI8SVcz2/10$iElppu6DicE7qd3CKiVISN47Ep/IaJoeJL4pvMns9.3";
+    isNormalUser = true;
+    description = "al";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "libvirtd"
+      "flatpak"
+      "audio"
+      "video"
+      "plugdev"
+      "input"
+      "qemu-libvirtd"
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqyyU56DPcUBpsvOxSZmLSvfxj5ltrNNR0sFMeOe4Xl alex@alns"
+    ];
+    packages = [ inputs.home-manager.packages.${pkgs.system}.home-manager ];
+  };
+  home-manager.users.al = import ../../../home/al/${config.networking.hostName}.nix;
 }
