d1954936bfb27e3f3ce70df1c7442c0d5c369295
kaian0n <linsenmannalex@proton.me>
2026-01-29 16:12:46 -0700

    plz

--- hosts/alns/services/cloudflared.nix
+++ hosts/alns/services/cloudflared.nix
@@ -15,17 +15,30 @@
       wants = [ "network-online.target" ];
       wantedBy = [ "multi-user.target" ];
 
+      environment = {
+         # Force Go resolver to use these DNS servers
+         GODEBUG = "netdns=go";
+      };
+
+      serviceConfig = {
+         Restart = "on-failure";
+         RestartSec = "5s";
+         # Use systemd's DNS override for this unit
+         BindReadOnlyPaths = [
+            "${pkgs.writeText "resolv.conf" ''
+               nameserver 8.8.8.8
+               nameserver 1.1.1.1
+            ''}:/etc/resolv.conf"
+         ];
+      };
+
       script = ''
          TOKEN=$(cat ${config.age.secrets.cloudflared-token.path})
          exec ${pkgs.cloudflared}/bin/cloudflared tunnel \
            --no-autoupdate \
            --edge-ip-version 4 \
+           --protocol http2 \
            run --token "$TOKEN"
       '';
-
-      serviceConfig = {
-         Restart = "on-failure";
-         RestartSec = "5s";
-      };
    };
 }
